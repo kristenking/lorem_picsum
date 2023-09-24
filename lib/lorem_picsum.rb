d8ff6a316ecff3f8624d0385709b4cbb70b6a4f3
kristenking <kristenking0110@gmail.com>
2023-09-23 22:11:43 -0400

    Initialize gem scaffold

---
+++ lib/lorem_picsum.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "lorem_picsum/version"
+
+module LoremPicsum
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/lorem_picsum_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe LoremPicsum do
+  it "has a version number" do
+    expect(LoremPicsum::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
